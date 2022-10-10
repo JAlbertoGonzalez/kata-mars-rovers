088c3a1fded84e5644080bec1934f3c20d60bc6a
Alberto Torre <45895462+JAlbertoGonzalez@users.noreply.github.com>
2022-10-10 22:07:55 +0200

    refactor render

--- src/index.js
+++ src/index.js
@@ -5,35 +5,47 @@ function matprint(mat) {
     mat.forEach(row => console.log(row.join(' ')))
 }
 
+function renderMap(map, position, direction) {
+    const x = position[0]
+    const y = position[1]
+
+    const arrow = {
+        [Rover.Facing.NORTH]: '^',
+        [Rover.Facing.SOUTH]: 'V',
+        [Rover.Facing.EAST]: '>',
+        [Rover.Facing.WEST]: '<'
+    }
+    
+    map[y][x] = arrow[direction]
+
+    return map
+}
+
+function printRoverPosition(rover) {
+    const currentPosition = rover.getPosition()
+    const map = rover.getMap()
+
+    renderMap(map, currentPosition, rover.direction)
+    matprint(map)
+}
+
 
 (() => {
     const rl = readline.createInterface(process.stdin, process.stdout);
-
     rl.setPrompt('ROVER> ')
     console.info('Press Control+C to exit')
 
     const rover = new Rover(12345, [10, 10])
+    printRoverPosition(rover)
 
     rl.prompt();
 
     rl.on('line', (data) => {
         rover.move(data)
 
-        const currentPosition = rover.getPosition()
-        const map = rover.getMap()
-        const x = currentPosition[0]
-        const y = currentPosition[1]
+        printRoverPosition(rover)
 
-        const arrow = {
-            [Rover.Facing.NORTH]: '^',
-            [Rover.Facing.SOUTH]: 'V',
-            [Rover.Facing.EAST]: '>',
-            [Rover.Facing.WEST]: '<'
-        }
-        
-        map[y][x] = arrow[rover.direction]
-        matprint(map)
-        console.log(currentPosition)
+        console.log('current position: ', rover.getPosition())
         rl.prompt()
     })
 
