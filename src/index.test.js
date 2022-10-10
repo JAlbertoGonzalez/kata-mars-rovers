396e1ecea18c32eb86a355181fc32fe357629f69
Alberto Torre <45895462+JAlbertoGonzalez@users.noreply.github.com>
2022-10-10 20:35:01 +0200

    fix tests

--- src/index.test.js
+++ src/index.test.js
@@ -9,39 +9,40 @@ describe('Mars Rovers', function () {
     it('should create grid', function () {
         const rover = new Rover(123, [13, 21])
         const map = rover.getMap()
-        expect(map[0]).toBe(13)
-        expect(map[1]).toBe(21)
+
+        expect(map.length).toBe(21)
+        expect(map[0].length).toBe(13)
     })
 
     it('should set initial position', () => {
         const rover1 = new Rover(123, [5, 5])
-        expect(rover1.getPosition()).toBe([0, 0])
+        expect(rover1.getPosition()).toStrictEqual([0, 0])
         const rover2 = new Rover(123, [5, 5], [2, 2])
-        expect(rover2.getPosition()).toBe([2, 2])
+        expect(rover2.getPosition()).toStrictEqual([2, 2])
         const rover3 = new Rover(123, [5, 5], [5, 5], Facing.NORTH)
-        expect(rover3.getPosition()).toBe([0, 0])
+        expect(rover3.getPosition()).toStrictEqual([0, 0])
     })
 
     it('should move', function () {
         const rover1 = new Rover(123, [5, 5], [1, 1], Facing.NORTH)
         rover1.move("F")
-        expect(rover1.getPosition()).toBe([1, 0])
+        expect(rover1.getPosition()).toStrictEqual([1, 0])
 
         const rover2 = new Rover(123, [5, 5], [1, 1], Facing.SOUTH)
         rover2.move("F")
-        expect(rover2.getPosition()).toBe([1, 2])
+        expect(rover2.getPosition()).toStrictEqual([1, 2])
     })
 
     it('should validate rover input commands', function () {
         const rover = new Rover(123, [13, 21])
-        expect(rover.move("FLFRBBBRRRLLLF")).toBeCalled()
-        expect(rover.move("INVALID COMMAND")).toThrow()
+        expect(rover.move("FLFRBBBRRRLLLF")).toBe(true)
+        expect(rover.move("INVALID COMMAND")).toBe(false)
     })
 
     it('should handle map boundaries on move', function () {
         const rover = new Rover(123, [4, 4], [0, 0], Facing.NORTH)
         rover.move("F")
-        expect(rover.getPosition()).toBe([0, 3])
+        expect(rover.getPosition()).toStrictEqual([0, 3])
     })
 
 })
