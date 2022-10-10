0db6c972de6a6ef45197ed5b7a820cb37b1492bd
Alberto Torre <45895462+JAlbertoGonzalez@users.noreply.github.com>
2022-10-10 21:55:26 +0200

    example usage script

--- src/index.js
+++ src/index.js
@@ -1,5 +1,40 @@
-import Rover from "./Rover"
+import Rover from "./Rover.js"
+import * as readline from 'readline'
+
+function matprint(mat) {
+    mat.forEach(row => console.log(row.join(' ')))
+}
+
 
 (() => {
-    const rover = new Rover(12345)
+    const rl = readline.createInterface(process.stdin, process.stdout);
+
+    rl.setPrompt('ROVER> ')
+    console.info('Press Control+C to exit')
+
+    const rover = new Rover(12345, [10, 10])
+
+    rl.prompt();
+
+    rl.on('line', (data) => {
+        rover.move(data)
+
+        const currentPosition = rover.getPosition()
+        const map = rover.getMap()
+        const x = currentPosition[0]
+        const y = currentPosition[1]
+
+        const arrow = {
+            [Rover.Facing.NORTH]: '^',
+            [Rover.Facing.SOUTH]: 'V',
+            [Rover.Facing.EAST]: '>',
+            [Rover.Facing.WEST]: '<'
+        }
+        
+        map[y][x] = arrow[rover.direction]
+        matprint(map)
+        console.log(currentPosition)
+        rl.prompt()
+    })
+
 })()
